1b7dae4a9e5228224e55add7a35846038131cfc6
tayseer1 <tayseer@vuedale.com>
2023-06-07 16:38:01 +0300

    asd

--- config/plugins.js
+++ config/plugins.js
@@ -4,24 +4,24 @@ module.exports =({env}) => ({
     //   enabled: true,
     //   resolve: './src/plugins/link-button'
     // },
-    // upload: {
-    //   config: {
-    //     provider: 'aws-s3',
-    //     providerOptions: {
-    //       accessKeyId: env('AWS_ACCESS_KEY_ID'),
-    //       secretAccessKey: env('AWS_ACCESS_SECRET'),
-    //       region: env('AWS_REGION'),
-    //       params: {
-    //         Bucket: env('AWS_BUCKET'),
-    //       },
-    //     },
-    //     actionOptions: {
-    //       upload: {},
-    //       uploadStream: {},
-    //       delete: {},
-    //     },
-    //   },
-    // },
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
     'preview-button': {
       config: {
         contentTypes: [
