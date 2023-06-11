b03bdf12963056848f6bbdacc4f7dddf30ad6c1f
tayseer1 <tayseer@vuedale.com>
2023-06-11 18:46:51 +0300

    asd

--- src/api/offer/controllers/offer.js
+++ src/api/offer/controllers/offer.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::offer.offer');
+module.exports = createCoreController('api::offer.offer', {
+    /**
+     * 
+     * @param {Object} ctx - Koa context
+     * @returns {Object}
+     */
+    async findOne(ctx) {
+      const { id } = ctx.body.uid;
+      const entity = await strapi.services.offer.findOne({ id });
+      return strapi.services.offer.sanitizeEntity(entity);
+    }
+  });
+
+const {sanitizeEntity} = require('strapi-utils')
+
+  
