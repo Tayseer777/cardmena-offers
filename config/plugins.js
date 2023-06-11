801cc02a781f78cfa79ed80dfb24d5863bc1bd45
tayseer1 <tayseer@vuedale.com>
2023-06-11 16:30:54 +0300

    asd

--- config/plugins.js
+++ config/plugins.js
@@ -35,7 +35,7 @@ module.exports =({env}) => ({
               },
             },
             published: {
-              url: 'https://cardmenaoffers.netlify.app/offers/{uid}',
+              // url: 'https://cardmenaoffers.netlify.app/offers/{uid}',
               url: 'https://vuedaleoffers.netlify.app/cardmena/{uid}',
             },
           },
