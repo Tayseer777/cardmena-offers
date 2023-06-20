b7cf442c1bd80cef2d5f8bee10b2c1e5bbbf0a66
tayseer1 <tayseer@vuedale.com>
2023-06-20 12:31:17 +0300

    any

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,33 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+   {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'naffasy.s3.us-east-1.amazonaws.com',
+            'http://localhost:8888/'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'naffasy.s3.us-east-1.amazonaws.com',
+            'http://localhost:8888/'
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
